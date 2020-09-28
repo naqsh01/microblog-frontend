94ef83415159aff739606be4102b1d189af43cf0
achauhancb <anandcpm@icloud.com>
2020-09-28 17:14:19 -0400

    Updated title v1.1.1

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -8,27 +8,16 @@ export const Flags = {
 }
 
 export const configurationFetchedHandler = fetcherResults => {
+  console.log('The configuration status is: ' + fetcherResults.fetcherStatus)
   if (fetcherResults.hasChanges && fetcherResults.fetcherStatus === 'APPLIED_FROM_NETWORK') {
     window.location.reload(false)
-  }
-}
-
-export const impressionHandler = (reporting, experiment) => {
-  console.log(experiment)
-  if (experiment.name === 'title') {
-    console.log('Title flag, ' + reporting.name + ' ,value is ' + reporting.value)
-    gtag('event', experiment.name, {
-      'event_category': reporting.name,
-      'event_label': reporting.value
-    })
-  } else {
-    console.log('Not in title experiment. Flag ' + reporting.name + '. default value ' + reporting.value + ' was used')
+  } else if (fetcherResults.fetcherStatus === 'ERROR_FETCH_FAILED') {
+    console.log('Error occured! Details are: ' + fetcherResults.errorDetails)
   }
 }
 
 const options = {
-  configurationFetchedHandler: configurationFetchedHandler,
-  impressionHandler: impressionHandler
+  configurationFetchedHandler: configurationFetchedHandler
 }
 
 Rox.setCustomBooleanProperty('isLoggedIn', store.getters.isLoggedIn)
